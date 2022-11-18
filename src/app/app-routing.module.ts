94e77492b7172a46a1195e2e6423750aeb5e8242
Bere Fraire Heredia <berenicefraireh@outlook.com>
2022-11-17 23:38:36 -0600

    Adding new modules and routes, login stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './login/login.module';
 
-const routes: Routes = [];
+// Components
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', loadChildren: () => import('./login/login.module').then(m => LoginModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
